c2f59600e226d1a2305f2309f54895366bdb749f
jiwonny <jiwon@daangn.com>
2024-08-07 19:49:11 +0900

    3-8

--- src/main/kotlin/chapter3/jiwon.kt
+++ src/main/kotlin/chapter3/jiwon.kt
@@ -6,3 +6,14 @@ fun takeSequence(n: Int, sequence: Sequence<Int>): List<Int> = when {
     sequence.none() -> listOf()
     else -> listOf(sequence.first()) + takeSequence(n - 1, sequence.drop(1))
 }
+
+// 3-8
+fun quickSort(list: List<Int>): List<Int> = when {
+    list.isEmpty() -> emptyList<Int>()
+    else -> {
+        val pivot = list.head()
+        val rest = list.tail()
+        val (smaller, bigger) = rest.partition { it < pivot }
+        quickSort(smaller) + pivot + quickSort(bigger)
+    }
+}
--- src/test/kotlin/chapter3/JiwonKtTest.kt
+++ src/test/kotlin/chapter3/JiwonKtTest.kt
@@ -1,6 +1,7 @@
 package chapter3
 
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 
 class JiwonKtTest : FreeSpec({
@@ -18,6 +19,23 @@ class JiwonKtTest : FreeSpec({
         "n이 sequence 개수보다 클 때" {
             val result = takeSequence(4, sequenceOf(1, 2, 3))
             result shouldBe listOf(1, 2, 3)
+        }
+    }
+
+    "quickSort" - {
+        "빈 list를 정렬한다" {
+            val result = quickSort(listOf())
+            result shouldBe emptyList()
+        }
+
+        "원소 하나 list를 정렬한다" {
+            val result = quickSort(listOf(1))
+            result shouldBe listOf(1)
+        }
+
+        "무작위 순서의 원소를 정렬한다" {
+            val result = quickSort(listOf(4, 5, 2, 1, 3))
+            result shouldBe listOf(1, 2, 3, 4, 5)
         }
     }
 })
